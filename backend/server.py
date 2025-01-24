68fe1580f3863f5bad66cfc10627a3a576162cfe
Leo Cheng <leocheng333375749@gmail.com>
2025-01-24 16:43:20 -0500

    server endpoints

--- backend/server.py
+++ backend/server.py
@@ -4,28 +4,26 @@ import time
 app = Flask(__name__)
 
 
-@app.route('/test_post', methods=['POST'])
-def post():
+@app.route('/submit', methods=['POST'])
+def submit():
     data = request.get_json()
-    gesture = "aaaa"
+    # data['type'], data['city'], data['country'], data['user']
+    date = time.strftime('%Y-%m-%d %H:%M:%S')
 
-    print('gesture :: ', gesture)
-    response = {
-        'message': 'post test',
-        'data': gesture
-    }
+    # add data to database
+
+    response = {'status': '200'}
     return jsonify(response)
 
 
-@app.route('/test_get', methods=['GET'])
-def get():
-    response = {
-        'message': 'get test',
-        'data': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    }
+@app.route('/data:<string:city>', methods=['GET'])
+def get(city):
+    # get data from database
+
+    response = {'data': "PUT DATA HERE"}
     return jsonify(response)
+
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
-    # app.get()
