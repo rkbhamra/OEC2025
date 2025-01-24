3bc7ea0682441c2d5308d065e8cbb0efeabcfb37
Nicko2102 <nicholas@witzel-milan.com>
2025-01-24 16:23:58 -0500

    pushing location getter and report adding function

--- backend/queries.py
+++ backend/queries.py
@@ -15,6 +15,10 @@ def getCategories():
     curL.execute(f"SELECT * FROM disastertypes ORDER BY Id = 0, DisasterType")
     print(curL.fetchall())
 
-# def report(GPSpos, type, etc)
+def report(GPSpos, dtype, city,  prov, country, severity, time):
+    curL.execute("INSERT into reports (City, Province, Country, Type, Severity, TotalReports, Latitude, Longitude, TimeOfReport) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+                (city, prov, country, dtype, severity, 1, GPSpos[0], GPSpos[1], time))
+
 
 getCategories()
+report([43.641681, -79.459243], 1, "Toronto", "ON", "Canada", 4, "2025-01-14 16:33:00")
