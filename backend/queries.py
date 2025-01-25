2524ef84480f45046b31774471d337436ad6fc52
Nicko2102 <nicholas@witzel-milan.com>
2025-01-24 19:15:43 -0500

    started looking for similar

--- backend/queries.py
+++ backend/queries.py
@@ -11,10 +11,10 @@ cnx = secureConnect("disastertracker")
 curD = cnx.cursor(dictionary=True)
 curL = cnx.cursor()
 
-def getCategories():
-    curL.execute(f"SELECT * FROM disastertypes ORDER BY Id = 0, DisasterType")
-    # print(curL.fetchall())
-    return curL.fetchall()
+# def getCategories():
+#     curL.execute(f"SELECT * FROM disastertypes ORDER BY Id = 0, DisasterType")
+#     # print(curL.fetchall())
+#     return curL.fetchall()
 
 def report(lat, long, dtype, city,  prov, country, severity, time):
     curL.execute("INSERT into reports (City, Province, Country, Type, Severity, TotalReports, Latitude, Longitude, TimeOfReport) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", 
@@ -22,11 +22,18 @@ def report(lat, long, dtype, city,  prov, country, severity, time):
     cnx.commit()
 
 def getTopNearMe(city):
-    curL.execute(f"SELECT r.*, t.disastertype FROM reports r INNER JOIN disastertypes t ON t.Id = r.Type WHERE CITY = \"{city}\" ORDER BY TotalReports DESC LIMIT 5")
+    curL.execute(f"SELECT * FROM reports WHERE CITY = \"{city}\" ORDER BY TotalReports DESC LIMIT 5")
     # print(curL.fetchall())
     return curL.fetchall()
+
+def checkForSimilar(type, lat, long):
+    curL.execute(f"SELECT * FROM reports where type = {type} AND abs(Latitude - {lat}) < 0.5 AND abs(Longitude - {long}) < 0.5")
+
+    ret = curL.fetchall()
+    print(ret)
+    return ret
     
 
 
-getCategories()
+# getCategories()
 # report([43.641681, -79.459243], 1, "Toronto", "ON", "Canada", 4, "2025-01-14 16:33:00")
